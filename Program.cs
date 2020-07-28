2d9293f4b23bf783aaf7ae07754c722eafb90fe0
Aykhan Alifov <aykhan.alifov@atltech.az>
2020-07-28 19:42:33 +0400

    >

--- Program.cs
+++ Program.cs
@@ -16,6 +16,7 @@ namespace weatherapi
             CreateHostBuilder(args).Build().Run();
         }
 
+
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
